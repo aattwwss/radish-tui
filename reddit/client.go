414f66f8d699aac1eb40a13a142953f80d567f4b
alvin.tan <alvintan95@gmail.com>
2024-04-18 00:53:28 +0800

    add reddit package:

---
+++ reddit/client.go
@@ -0,0 +1,149 @@
+package reddit
+
+import (
+	"encoding/json"
+	"errors"
+	"io"
+	"net/http"
+	"net/url"
+	"strings"
+	"time"
+
+	"github.com/rs/zerolog/log"
+)
+
+type tokenRes struct {
+	AccessToken string `json:"access_token"`
+	TokenType   string `json:"token_type"`
+	ExpiresIn   int    `json:"expires_in"`
+	Scope       string `json:"scope"`
+}
+
+type Client struct {
+	httpClient     *http.Client
+	clientId       string
+	clientSecret   string
+	username       string
+	password       string
+	overrideOldBot bool
+
+	accessToken          string
+	tokenExpireTimeMilli int64
+}
+
+const (
+	userAgent = "Reddit_Radish_TUI/0.1" //need to set user agent to prevent getting blocked by reddit
+)
+
+func NewRedditClient(clientId, clientSecret, username, password, accessToken string, expireTimeMilli int64, overrideOldBot bool) (*Client, error) {
+	if clientId == "" || clientSecret == "" || username == "" || password == "" {
+		return nil, errors.New("clientId, clientSecret, username, password cannot be empty")
+	}
+
+	httpClient := &http.Client{
+		Timeout: time.Second * 10,
+	}
+
+	rc := Client{
+		httpClient:           httpClient,
+		clientId:             clientId,
+		clientSecret:         clientSecret,
+		username:             username,
+		password:             password,
+		overrideOldBot:       overrideOldBot,
+		accessToken:          accessToken,
+		tokenExpireTimeMilli: expireTimeMilli,
+	}
+
+	err := rc.RefreshToken()
+	if err != nil {
+		return nil, err
+	}
+
+	return &rc, nil
+}
+
+func (rc *Client) RefreshToken() error {
+	now := time.Now()
+	durationFromExpire := time.UnixMilli(rc.tokenExpireTimeMilli).Sub(now).Minutes()
+	if durationFromExpire > 30 {
+		return nil
+	}
+	// Set the form data
+	data := url.Values{}
+	data.Set("grant_type", "password")
+	data.Set("username", rc.username)
+	data.Set("password", rc.password)
+
+	// Create a new POST request
+	req, err := http.NewRequest("POST", "https://www.reddit.com/api/v1/access_token", strings.NewReader(data.Encode()))
+	req.Header.Add("User-Agent", userAgent)
+	if err != nil {
+		log.Error().Msgf("Error creating request: %v", err)
+		return err
+	}
+
+	// Set the content type header
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Set("Accept", "*/*")
+	req.SetBasicAuth(rc.clientId, rc.clientSecret)
+
+	// Send the request
+	resp, err := retryHttpRequest(rc.httpClient, req, 5, time.Minute)
+	if err != nil {
+		log.Error().Msgf("Error sending request: %v", err)
+		return err
+	}
+	if resp.StatusCode/100 != 2 {
+		log.Error().Msgf("Error request: %v", resp.Status)
+		return errors.New("Received non OK status code: " + resp.Status)
+	}
+
+	defer resp.Body.Close()
+
+	var tokenRes tokenRes
+	err = json.NewDecoder(resp.Body).Decode(&tokenRes)
+	if err != nil {
+		log.Error().Msgf("Error decoding response body:", err)
+		return err
+	}
+
+	rc.accessToken = tokenRes.AccessToken
+	rc.tokenExpireTimeMilli = now.Add(time.Duration(tokenRes.ExpiresIn) * time.Second).UnixMilli()
+
+	return nil
+}
+
+func (rc *Client) newRequest(method string, url string, body io.Reader) (*http.Request, error) {
+	err := rc.RefreshToken()
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequest(method, url, body)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Add("Authorization", "bearer "+rc.accessToken)
+	req.Header.Add("User-Agent", userAgent)
+	return req, nil
+}
+
+func retryHttpRequest(client *http.Client, req *http.Request, attempts int, sleep time.Duration) (*http.Response, error) {
+	var resp *http.Response
+	var err error
+
+	for i := 0; i < attempts; i++ {
+		resp, err = client.Do(req)
+		if err == nil || resp.StatusCode/100 == 2 {
+			return resp, nil
+		}
+
+		log.Error().Msgf("Error sending request: %v", err)
+		time.Sleep(sleep)
+		sleep *= 2 // increase delay exponentially
+	}
+
+	return nil, errors.New("http request exceeded retry attempts")
+}
---
+++ reddit/submission.go
@@ -0,0 +1,129 @@
+package reddit
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/url"
+	"strings"
+
+	"github.com/rs/zerolog/log"
+)
+
+type Submission struct {
+	ID            string `json:"id"`
+	Subreddit     string `json:"subreddit"`
+	Title         string `json:"title"`
+	Name          string `json:"name"`
+	LinkFlairText string `json:"link_flair_text"`
+}
+
+func (rc *Client) GetNewSubmissions(subreddit string, limit int) ([]Submission, error) {
+	redditUrl := fmt.Sprintf("https://oauth.reddit.com/r/%s/new?limit=%v", subreddit, limit)
+	req, err := rc.newRequest("GET", redditUrl, nil)
+	if err != nil {
+		log.Error().Msgf("Error creating request: %v", err)
+		return nil, err
+	}
+	resp, err := rc.httpClient.Do(req)
+	if err != nil {
+		log.Error().Msgf("Error sending request: %v", err)
+		return nil, err
+	}
+	if resp.StatusCode/100 != 2 {
+		log.Error().Msgf("Error request: %v", resp.Status)
+		return nil, errors.New("Received non OK status code: " + resp.Status)
+	}
+	defer resp.Body.Close()
+
+	var listings Listing[Submission]
+	err = json.NewDecoder(resp.Body).Decode(&listings)
+	if err != nil {
+		log.Error().Msgf("Error decoding response body:", err)
+		return nil, err
+	}
+	var posts []Submission
+	for _, child := range listings.Data.Children {
+		posts = append(posts, child.Data)
+	}
+	return posts, nil
+}
+
+type SubmissionComment struct {
+	SubredditID    string `json:"subreddit_id"`
+	Subreddit      string `json:"subreddit"`
+	ID             string `json:"id"`
+	Author         string `json:"author"`
+	ParentID       string `json:"parent_id"`
+	AuthorFullname string `json:"author_fullname"`
+	Body           string `json:"body"`
+	Name           string `json:"name"`
+	IsSubmitter    bool   `json:"is_submitter"`
+}
+
+func (rc *Client) GetCommentsBySubmissionId(submissionId string, limit int) ([]SubmissionComment, error) {
+	redditUrl := fmt.Sprintf("https://oauth.reddit.com/comments/%s?limit=%v&depth=1", submissionId, limit)
+	req, err := rc.newRequest("GET", redditUrl, nil)
+	if err != nil {
+		log.Error().Msgf("Error creating request: %v", err)
+		return nil, err
+	}
+	resp, err := rc.httpClient.Do(req)
+	if err != nil {
+		log.Error().Msgf("Error sending request: %v", err)
+		return nil, err
+	}
+	if resp.StatusCode/100 != 2 {
+		log.Error().Msgf("Error request: %v", resp.Status)
+		return nil, errors.New("Received non OK status code: " + resp.Status)
+	}
+	defer resp.Body.Close()
+
+	var listings []Listing[SubmissionComment]
+	err = json.NewDecoder(resp.Body).Decode(&listings)
+	if err != nil {
+		log.Error().Msgf("Error decoding response body:", err)
+		return nil, err
+	}
+	var submissionComments []SubmissionComment
+	// hardcoding to use the 2nd element as the first is the post information
+	for _, child := range listings[1].Data.Children {
+		submissionComments = append(submissionComments, child.Data)
+	}
+	return submissionComments, nil
+}
+
+func (rc *Client) SubmitComment(postId, text string) error {
+	data := url.Values{}
+	data.Set("api_type", "json")
+	data.Set("text", text)
+	data.Set("thing_id", "t3_"+postId)
+
+	req, err := rc.newRequest("POST", "https://oauth.reddit.com/api/comment", strings.NewReader(data.Encode()))
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	if err != nil {
+		log.Error().Msgf("Error creating request: %v", err)
+		return err
+	}
+	resp, err := rc.httpClient.Do(req)
+	if err != nil {
+		log.Error().Msgf("Error sending request: %v", err)
+		return err
+	}
+	if resp.StatusCode/100 != 2 {
+		log.Error().Msgf("Error request: %v", resp.Status)
+		return errors.New("Received non OK status code: " + resp.Status)
+	}
+	defer resp.Body.Close()
+	return nil
+}
+
+func (rc *Client) IsCommentedByUser(submissionId string, author string) bool {
+	comments, _ := rc.GetCommentsBySubmissionId(submissionId, 100)
+	for _, comment := range comments {
+		if comment.Author == author {
+			return true
+		}
+	}
+	return false
+}
