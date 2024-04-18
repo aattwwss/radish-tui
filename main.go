f806b82814fa34a733133c783385964441accced
alvin.tan <alvintan95@gmail.com>
2024-04-18 17:16:48 +0800

    test tea list

--- main.go
+++ main.go
@@ -2,9 +2,14 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"strings"
 
 	"github.com/aattwwss/radish-tui/reddit"
 	"github.com/caarlos0/env/v8"
+	"github.com/charmbracelet/bubbles/list"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 	"github.com/joho/godotenv"
 	"github.com/rs/zerolog/log"
 )
@@ -20,6 +25,74 @@ type Config struct {
 	Token           string `env:"BOT_ACCESS_TOKEN"`
 	ExpireTimeMilli int64  `env:"BOT_TOKEN_EXPIRE_MILLI"`
 	IsDebug         bool   `env:"IS_DEBUG"`
+}
+
+type model struct {
+	list list.Model
+}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.WindowSizeMsg:
+		m.list.SetWidth(msg.Width)
+		return m, nil
+
+	case tea.KeyMsg:
+		switch keypress := msg.String(); keypress {
+		case "q", "ctrl+c":
+			return m, tea.Quit
+		}
+	}
+
+	var cmd tea.Cmd
+	m.list, cmd = m.list.Update(msg)
+	return m, cmd
+}
+
+func (m model) View() string {
+	return "\n" + m.list.View()
+}
+
+type item string
+
+func (i item) FilterValue() string { return "" }
+
+const listHeight = 14
+
+var (
+	titleStyle        = lipgloss.NewStyle().MarginLeft(2)
+	itemStyle         = lipgloss.NewStyle().PaddingLeft(4)
+	selectedItemStyle = lipgloss.NewStyle().PaddingLeft(2).Foreground(lipgloss.Color("170"))
+	paginationStyle   = list.DefaultStyles().PaginationStyle.PaddingLeft(4)
+	helpStyle         = list.DefaultStyles().HelpStyle.PaddingLeft(4).PaddingBottom(1)
+	quitTextStyle     = lipgloss.NewStyle().Margin(1, 0, 2, 4)
+)
+
+type itemDelegate struct{}
+
+func (d itemDelegate) Height() int                             { return 1 }
+func (d itemDelegate) Spacing() int                            { return 0 }
+func (d itemDelegate) Update(_ tea.Msg, _ *list.Model) tea.Cmd { return nil }
+func (d itemDelegate) Render(w io.Writer, m list.Model, index int, listItem list.Item) {
+	i, ok := listItem.(item)
+	if !ok {
+		return
+	}
+
+	str := fmt.Sprintf("%d. %s", index+1, i)
+
+	fn := itemStyle.Render
+	if index == m.Index() {
+		fn = func(s ...string) string {
+			return selectedItemStyle.Render("> " + strings.Join(s, " "))
+		}
+	}
+
+	fmt.Fprint(w, fn(str))
 }
 
 func main() {
@@ -41,8 +114,23 @@ func main() {
 	if err != nil {
 		log.Error().Msgf("error: %v", err)
 	}
+	items := []list.Item{}
 	for _, submission := range submissions {
-		fmt.Println(submission.Title)
+		items = append(items, item(submission.Title))
+	}
+	const defaultWidth = 20
+
+	l := list.New(items, itemDelegate{}, defaultWidth, listHeight)
+	l.Title = "What do you want for dinner?"
+	l.SetShowStatusBar(false)
+	l.SetFilteringEnabled(false)
+	l.Styles.Title = titleStyle
+	l.Styles.PaginationStyle = paginationStyle
+	l.Styles.HelpStyle = helpStyle
+	m := model{list: l}
+
+	if _, err := tea.NewProgram(m).Run(); err != nil {
+		log.Fatal().Msgf("Error running program: %v", err)
 	}
 
 }
